7e63f9b6f0fd99680165b231c86bba7da6aca7b9
blaxxun <blaxxun@blaxxun.blaxxun>
2021-11-14 14:41:11 +0100

    add upgrade requirements

--- ItemManager/ItemManager.cs
+++ ItemManager/ItemManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -66,6 +67,12 @@ namespace ItemManager
 		/// </summary>
 		public readonly RequiredResourceList RequiredItems = new();
 		/// <summary>
+		/// Specifies the resources needed to upgrade the item.
+		/// <para>Use .Add to add resources with their internal ID and an amount. This amount will be multipled by the item quality level.</para>
+		/// <para>Use one .Add for each resource type the item should need.</para>
+		/// </summary>
+		public readonly RequiredResourceList RequiredUpgradeItems = new();
+		/// <summary>
 		/// Specifies the crafting station needed to craft the item.
 		/// <para>Use .Add to add a crafting station, using the CraftingTable enum and a minimum level for the crafting station.</para>
 		/// <para>Use one .Add for each crafting station.</para>
@@ -95,6 +102,17 @@ namespace ItemManager
 
 			foreach (Item item in registeredItems)
 			{
+				ItemDrop ResItem(Requirement r) => __instance.GetItemPrefab(r.itemName).GetComponent<ItemDrop>();
+				Dictionary<string, Piece.Requirement> resources = item.RequiredItems.Requirements.ToDictionary(r => r.itemName, r => new Piece.Requirement { m_amount = r.amount, m_resItem = ResItem(r), m_amountPerLevel = 0 });
+				foreach (Requirement req in item.RequiredUpgradeItems.Requirements)
+				{
+					if (!resources.TryGetValue(req.itemName, out Piece.Requirement requirement))
+					{
+						requirement = resources[req.itemName] = new Piece.Requirement { m_resItem = ResItem(req), m_amount = 0 };
+					}
+					requirement.m_amountPerLevel = req.amount;
+				}
+
 				foreach (CraftingStationConfig station in item.Crafting.Stations)
 				{
 					Recipe recipe = ScriptableObject.CreateInstance<Recipe>();
@@ -102,11 +120,29 @@ namespace ItemManager
 					recipe.m_amount = item.CraftAmount;
 					recipe.m_enabled = true;
 					recipe.m_item = item.Prefab.GetComponent<ItemDrop>();
-					recipe.m_resources = item.RequiredItems.Requirements.Select(r => new Piece.Requirement { m_amount = r.amount, m_resItem = __instance.GetItemPrefab(r.itemName).GetComponent<ItemDrop>() }).ToArray();
+					recipe.m_resources = resources.Values.ToArray();
 					recipe.m_craftingStation = ZNetScene.instance.GetPrefab(((InternalName)typeof(CraftingTable).GetMember(station.Table.ToString())[0].GetCustomAttributes(typeof(InternalName)).First()).internalName).GetComponent<CraftingStation>();
 					recipe.m_minStationLevel = station.level;
 
 					__instance.m_recipes.Add(recipe);
+				}
+			}
+		}
+
+		private static bool CheckItemIsUpgrade(InventoryGui gui) => gui.m_selectedRecipe.Value?.m_quality > 0;
+		
+		internal static IEnumerable<CodeInstruction> Transpile_InventoryGui(IEnumerable<CodeInstruction> instructions)
+		{
+			List<CodeInstruction> instrs = instructions.ToList();
+			FieldInfo amountField = AccessTools.DeclaredField(typeof(Recipe), nameof(Recipe.m_amount));
+			for (int i = 0; i < instrs.Count; ++i)
+			{
+				yield return instrs[i];
+				if (i > 1 && instrs[i - 2].opcode == OpCodes.Ldfld && instrs[i - 2].OperandIs(amountField) && instrs[i - 1].opcode == OpCodes.Ldc_I4_1 && instrs[i].operand is Label)
+				{
+					yield return new CodeInstruction(OpCodes.Ldarg_0);
+					yield return new CodeInstruction(OpCodes.Call, AccessTools.DeclaredMethod(typeof(Item), nameof(CheckItemIsUpgrade)));
+					yield return new CodeInstruction(OpCodes.Brtrue, instrs[i].operand);
 				}
 			}
 		}
@@ -117,10 +153,12 @@ namespace ItemManager
 		static PrefabManager()
 		{
 			Harmony harmony = new("org.bepinex.helpers.ItemManager");
-			harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB)), new HarmonyMethod(AccessTools.DeclaredMethod(typeof(PrefabManager), nameof(Patch_ObjectDBInit))));
+			harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB)), postfix: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(PrefabManager), nameof(Patch_ObjectDBInit))));
 			harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.Awake)), new HarmonyMethod(AccessTools.DeclaredMethod(typeof(PrefabManager), nameof(Patch_ObjectDBInit))));
 			harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB)), new HarmonyMethod(AccessTools.DeclaredMethod(typeof(Item), nameof(Item.Patch_ObjectDBInit))));
 			harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.Awake)), new HarmonyMethod(AccessTools.DeclaredMethod(typeof(Item), nameof(Item.Patch_ObjectDBInit))));
+			harmony.Patch(AccessTools.DeclaredMethod(typeof(ZNetScene), nameof(ZNetScene.Awake)), new HarmonyMethod(AccessTools.DeclaredMethod(typeof(PrefabManager), nameof(Patch_ZNetSceneAwake))));
+			harmony.Patch(AccessTools.DeclaredMethod(typeof(InventoryGui), nameof(InventoryGui.UpdateRecipe)), transpiler: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(Item), nameof(Item.Transpile_InventoryGui))));
 		}
 		
 		private struct BundleId
@@ -157,18 +195,23 @@ namespace ItemManager
 		[HarmonyPriority(Priority.VeryHigh)]
 		private static void Patch_ObjectDBInit(ObjectDB __instance)
 		{
-			if (__instance.GetItemPrefab("Wood") == null)
-			{
-				return;
-			}
-
 			foreach (GameObject prefab in prefabs)
 			{
-				__instance.m_items.Add(prefab);
-				ZNetScene.instance.m_prefabs.Add(prefab);
-				ZNetScene.instance.m_namedPrefabs.Add(prefab.name.GetStableHashCode(), prefab);
+				if (!__instance.m_items.Contains(prefab))
+				{
+					__instance.m_items.Add(prefab);
+				}
 			}
 			__instance.UpdateItemHashes();
+		}
+		
+		[HarmonyPriority(Priority.VeryHigh)]
+		private static void Patch_ZNetSceneAwake(ZNetScene __instance)
+		{
+			foreach (GameObject prefab in prefabs)
+			{
+				__instance.m_prefabs.Add(prefab);
+			}
 		}
 	}
 }
